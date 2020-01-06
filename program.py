dcbf9619dc420d2c0d134615416f8e0a5e9ed1c9
Jeroen Bouma <jer.bouma@gmail.com>
2020-01-06 22:28:27 +0100

    Added an error message when a crash occurs

--- program.py
+++ program.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import messagebox
 from report import excelReport
 from PIL import ImageTk
 from PIL import Image
@@ -25,7 +26,7 @@ class Window(Frame):
         self.master.title("The Passive Investor")
         self.pack(fill=BOTH, expand=1)
 
-        image = ImageTk.PhotoImage(Image.open(resource_path("images/ThePassiveInvestorPNG.png")))
+        image = ImageTk.PhotoImage(Image.open(resourcePath("images\ThePassiveInvestorPNG.png")))
         panel = Label(self, image = image, bg=background)
         panel.image = image
         panel.grid(row=1,column=1, columnspan=2,sticky=W+E+N+S)
@@ -53,22 +54,29 @@ class Window(Frame):
         excelReportButton.grid(row=4,column=1, columnspan=2, sticky=W+E+N+S,padx=10, pady=10)
 
     def generateReport(self):
-        screenerURL = self.screenerEntry.get()
+        try:
+            screenerURL = self.screenerEntry.get()
 
-        if self.filenameEntry.get()[-5:] == '.xlsx':
-            filename = self.filenameEntry.get()
-        else:
-            filename = self.filenameEntry.get() + '.xlsx'
+            if self.filenameEntry.get()[-5:] == '.xlsx':
+                filename = self.filenameEntry.get()
+            else:
+                filename = self.filenameEntry.get() + '.xlsx'
 
-        progress = Label(self, text="Collecting tickers..",bg=buttonColor,fg=buttonTextColour)
-        progress.grid(row=4,column=1, columnspan=2, sticky=W+E+N+S,padx=10, pady=10)
-        tickers = symbolCollector(screenerURL)
+            progress = Label(self, text="Collecting tickers..",bg=buttonColor,fg=buttonTextColour)
+            progress.grid(row=4,column=1, columnspan=2, sticky=W+E+N+S,padx=10, pady=10)
+            tickers = symbolCollector(screenerURL)
 
-        excelReport.excelWriter(self, tickers, filename)
+            excelReport.excelWriter(self, tickers, filename)
 
+        except Exception as error:
+            messagebox.showerror('Error', "The program has crashed with the following error: \n\n"
+            + str(error) + "\n\nIf the problem persists, please create an Issue with the error "
+            "message on the project's GitHub page: https://github.com/JerBouma/ThePassiveInvestor/issues. \n\n"
+            + "You can copy this entire message with CTRL + C.")
+        
         excelReportButton = Button(self, text="Create Report", command=self.runProgram,bg=buttonColor,fg=buttonTextColour)
         excelReportButton.grid(row=4,column=1, columnspan=2, sticky=W+E+N+S,padx=10, pady=10)
-    
+
     def runProgram(self):
         threading.Thread(target=self.generateReport).start()
         
