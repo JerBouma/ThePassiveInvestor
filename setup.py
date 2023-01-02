4f80596e84935655623738ee926ed5ffc458a3c7
Jeroen Bouma <jeroenbouma@Jeroens-MacBook-Pro.local>
2023-01-02 17:25:04 +0100

    Release a new version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 setuptools.setup(
     name="thepassiveinvestor",
     packages=["thepassiveinvestor"],
-    version="1.0.10",
+    version="1.0.11",
     license="MIT",
     description="Passive Investing for the Average Joe.",
     author="JerBouma",
